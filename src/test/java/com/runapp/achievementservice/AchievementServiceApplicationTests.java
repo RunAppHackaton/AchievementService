71dcf72badcb4993270edb92d98da1e3fea071f5
Denis Durbalov <den.dyrbalov25@gmail.com>
2023-09-13 10:21:51 +0200

    first commit

---
+++ src/test/java/com/runapp/achievementservice/AchievementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runapp.achievementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchievementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
